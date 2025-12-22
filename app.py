10a787e795ecb554fd24cf6b738fa803fdea9deb
Fırat <firatoz412@gmail.com>
2025-12-22 10:30:00 +0300

    app.py dosyası güncellendi

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask,render_template,flash,redirect,session,url_for
+from flask import Flask,render_template
 import os 
 from dotenv import load_dotenv
 
 
 from routes.book import book_bp
 from routes.auth import auth_bp
-
+from routes.admin import admin_bp
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -15,17 +15,16 @@ app.secret_key = os.getenv("SECRET_KEY")
 
 app.register_blueprint(book_bp)
 app.register_blueprint(auth_bp)
+app.register_blueprint(admin_bp)
 
 
 
 @app.route("/")
 def index():
-    if "user_id" not in session:
-            flash("Bu sayfaya erişmek için giriş yapmalısınız.", "warning")
-            return redirect(url_for("auth.login"))
     return render_template("index.html")
     
 
-
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+    
+    
